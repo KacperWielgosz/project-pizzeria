186fd8f14a0f9ef8ce38195155c4a232f4930149
K W <k4cperwi3lgosz@gmail.com>
2022-01-19 14:06:03 +0100

    Mod 8

--- src/js/script.js
+++ src/js/script.js
@@ -54,7 +54,7 @@
 
   const app = {
     initMenu: function(){
-      thisApp = this;
+      const thisApp = this;
       console.log('thisApp.data:', thisApp.data);
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
@@ -62,7 +62,7 @@
     },
     initData: function(){
       const thisApp = this;
-      thisApp.data = dataSource
+      thisApp.data = dataSource;
     },
     init: function(){
       const thisApp = this;
@@ -78,20 +78,108 @@
   };
 
   class Product {
-    constructor (id, data) {
+    constructor (id, data){
       const thisProduct = this;
-      thisProduct.renderInMenu();
       thisProduct.id = id;
       thisProduct.data = data;
+      thisProduct.renderInMenu();
       console.log('new Product', thisProduct);
+
+      thisProduct.initAccordion();
+
+      initOrderForm()
+
+      processOrder()
     };
     renderInMenu(){
       const thisProduct = this;
       const generatedHTML = templates.menuProduct(thisProduct.data);
-      thisProduct.element - utils.createDOMFromHTML(generatedHTML);
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
       const menuContainer = document.querySelector(select.containerOf.menu);
       menuContainer.appendChild(thisProduct.element);
+    };
+    /*
+    getElements(){
+    const thisProduct = this;
+
+    thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+    thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
+    thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
+    thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
+    thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
     }
+    */
+    initAccordion(){
+      const thisProduct = this;
+      /* find the clickable trigger (the element that should react to clicking) */
+      const clickableTrigger = document.querySelector(select.menuProduct.clickable);
+      /* START: add event listener to clickable trigger on event click */
+      clickableTrigger.addEventListener('click', function(event) {
+      /* prevent default action for event */
+        event.preventDefault();
+      /* find active product (product that has active class) */
+        const activeProduct = document.querySelector(classNames.menuProduct.wrapperActive);
+      /* if there is active product and it's not thisProduct.element, remove class active from it */
+        if (thisProduct.element = activeProduct) {
+          console.log('activeProduct', activeProduct);
+        } else {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive)
+        };
+      /* toggle active class on thisProduct.element */
+      thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive)
+      });
+     };
+     /*
+     initOrderForm(){
+       const thisProduct = this;
+
+       const formData = utils.serializeFormToObject(thisProduct.form);
+       console.log('formData', formData);
+       thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+        });
+
+        for(let input of thisProduct.formInputs){
+          input.addEventListener('change', function(){
+          thisProduct.processOrder();
+          });
+        }
+
+        thisProduct.cartButton.addEventListener('click', function(event){
+          event.preventDefault();
+          thisProduct.processOrder();
+        });
+     };
+     processOrder(){
+     const thisProduct = this;
+
+     // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData', formData);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+        console.log(paramId, param);
+
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          console.log(optionId, option);
+          // 
+        }
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
+     };
+     */
   };
 
   app.init();
